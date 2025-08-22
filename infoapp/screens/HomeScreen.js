ec1225203cd224e4345e160b245fdde8914b1103
kkhw1264 <kkhw1264@gmail.com>
2025-08-22 13:59:37 +0800

    change 1

--- infoapp/screens/HomeScreen.js
+++ infoapp/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import {View, Text} from 'react-native';
+import {Card} from 'react-native-paper';
 
 const HomeScreen = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
